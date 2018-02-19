8bca82cd9f74ffa1de1c914bbde15ba50f95f80c
Niko <nikolai.berns@gmx.net>
2018-02-19 17:13:18 +0700

    perfect if keys added

--- bot-comment-on-tag.js
+++ bot-comment-on-tag.js
@@ -1,77 +1,59 @@
+// Hello.
+//
+// This is JSHint, a tool that helps to detect errors and potential
+// problems in your JavaScript code.
+//
+// To start, simply enter some JavaScript anywhere on this page. Your
+// report will appear on the right side.
+//
+// Additionally, you can toggle specific options in the Configure
+// menu.
+
 const steem = require('steem');
+//CONSTANTS AND VARIABLES
       var profit;
       var amountFloat;
       var ACCOUNT_NAME = '';
       var ACCOUNT_KEY = '';
+      const TAG = "teamnz";
       var permlink = new Date().toISOString().replace(/[^a-zA-Z0-9]+/g, '').toLowerCase();
-        console.log('Checking Transactions: ')
-        // Tutorial 05 - @mention Bot
-        const TX_NAME = 'minnowbooster'
-        steem.api.setOptions({ url: 'https://api.steemit.com' });
-        steem.api.streamTransactions('head', function(err, result) {
+//START
+      console.log('Checking Transactions: ');
+      steem.api.setOptions({ url: 'https://api.steemit.com' });
 
-          var txType = result.operations[0][0]
-          var txData = result.operations[0][1]
+//GET DATA FROM BLOCKCHAIN
+      steem.api.streamTransactions('head', function(err, result) {
+      var txType = result.operations[0][0];
+      var txData = result.operations[0][1];
+//Check that it is a post
+      if (txType=='comment'){
+          var author = txData.author;
+          var link = txData.permlink;
+          console.log('processing post by: ', author, ' link: ', link);
+					var json;
+          try {json = JSON.parse(txData.json_metadata);}
+          catch(err) {console.log('No json metadata: ', err.name);}
+          try {console.log('JSON: ', json.tags);}
+          catch(err){console.log('No tag object in json: ', err.name);}
+          var hasTag;
+          try {hasTag=json.tags.indexOf(TAG);}
+          catch(err) {console.log('Cannot call indexOf: ', err.name);}
 
-          if (txType=='comment'){
-
-            var author = txData.author;
-            var link = txData.permlink;
-          console.log('TXDATA: ', txData);
-          var json = JSON.parse(txData.json_metadata);
-          console.log('JSON: ', json.tags);
-         if (json.tags.indexOf("teamnz") > -1){
-          sendVote(ACCOUNT_KEY, ACCOUNT_NAME, author, permlink, 10000);
+          if (hasTag > -1){
+          console.log('Tag found in post by: ', author);
           postComment(ACCOUNT_NAME,ACCOUNT_KEY,permlink,author,link);
-          }
-        }
-//json_metadata: "{\"tags\":[\"photography\",\"castellano\",\"carnaval\",\"travel\",\"venezuela\"],\"image\":[\"https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/34806_150380245014518_7920146_n.jpg?oh=d97de59e2fa514dc5ef7aa86f2af0149&oe=5B258830\",\"https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/10398493_58079215926_4436441_n.jpg?oh=562702edd38b55144f6623d4d223ff03&oe=5B102CC5\",\"https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/10398493_58079230926_607391_n.jpg?oh=2ef2e0d716593289c3f183fb40bf204c&oe=5B150E82\",\"https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/165546_150382271680982_3329837_n.jpg?oh=43261d642c37d2cda6544f630d3c5def&oe=5B11186E\",\"https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9
+          sendVote(ACCOUNT_KEY, ACCOUNT_NAME, author, link, 10000);
 
+                         } // close if
+                }//close if=comment
+        }//close err funk
+    );//close streamTransactions
 
-          var includesMention = checkTransaction(txType,txData)
-          if (includesMention) {
-            sendEmail(txData)
-            getPriceFeed();
-          }
-        });
-        function getPriceFeed(){
-          $.getJSON('https://api.coinmarketcap.com/v1/ticker/steem-dollars/', function(data) {
-    var price = data[0].price_usd;
-    price = Number(price)
-    profit = amountFloat*price;
-  //  console.log('Pricefeed: ' + price.toFixed(2) + ' (USD/SBD) // Amount: ' + amountFloat + ' SBD // Profit: $' + profit.toFixed(2)+ ' USD');
-});
-        }
+        function sendVote(ACCOUNT_KEY, ACCOUNT_NAME,author, link, weight){
+            steem.broadcast.vote(ACCOUNT_KEY, ACCOUNT_NAME, author, link, weight, function(err, result) {
+                console.log(err, result);
+                console.log('Voted on post.');
 
-        function checkTransaction(txType,txData) {
-          if(txType == 'transfer') {
-
-            console.log('PROCESSING: ','from:' + txData.from, 'to: ' + txData.to)
-            var sender = txData.from;
-            var mentionname = '@' + TX_NAME;
-            if(txData.from == 'minnowbooster'){
-          //  console.log('Transfer identified!')
-          //  console.log (`Hey! Looks like @${txData.to} just got a payout from @${txData.from}`);
-        //    console.log (txData)
-            amountFloat = Number(txData.amount.match( /[\d|,|.|e|E|\+]+/g ))
-            }
-
-            return (txData.from == 'minnowbooster')
-          }
-        }
-        function sendEmail(txData){
-        //  console.log('SENDING EMAIL')
-          $.ajax({
-              url: 'https://formspree.io/nickb1893@gmail.com',
-              method: 'POST',
-              data: { message: `Hey! Looks like @${txData.to} just got a payout from @${txData.from}` },
-              dataType: 'json'
-          });
-        }
-        function sendVote(ACCOUNT_KEY, ACCOUNT_NAME,author, permlink, weight){
-            steem.broadcast.vote(ACCOUNT_KEY, ACCOUNT_NAME, author, permlink, weight, function(err, result) {
-                console.log(err, result)
-                console.log('***voted***');
             });
         }
         function postComment(ACCOUNT_NAME,ACCOUNT_KEY,permlink,author,link){
@@ -83,9 +65,11 @@ const steem = require('steem');
           permlink, // Permlink
           '', // Title
           '<a href="https://discord.gg/rXENHmb"><img src="https://steemitimages.com/DQmW1NKA8XygdJzHidCbnx8o6SsFDKirS3CYrwRLYmRkhWe/teamnz.jpg"></a><br><i>This is a curation bot for TeamNZ. Please join our AUS/NZ community on <a href="https://discord.gg/rXENHmb">Discord</a>.<br>For any inquiries about the bot please contact @cryptonik.</i>', // Body,
-          { tags: ['test'], app: `steemjs/examples` }, // Json Metadata
+          { tags: ['test'], app: 'teamnz/cryptonik' }, // Json Metadata
           function(err, result) {
             console.log(err, result);
-          }
-        );
-        }
+            console.log('Commented on post.');
+
+                                }
+                        );
+                }
