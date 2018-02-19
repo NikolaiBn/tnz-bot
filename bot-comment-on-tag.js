1e070e5cdb4a29e2dbd8b96195e03566a37033c1
Niko <nikolai.berns@gmx.net>
2018-02-19 15:41:26 +0700

    working

--- bot-comment-on-tag.js
+++ bot-comment-on-tag.js
@@ -1,8 +1,4 @@
-  <script src="https://cdn.steemjs.com/lib/latest/steem.min.js"></script>
-      <script src="https://code.jquery.com/jquery-3.2.1.min.js"
-  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-  crossorigin="anonymous"></script>
-      <script>
+const steem = require('steem');
       var profit;
       var amountFloat;
       var ACCOUNT_NAME = '';
@@ -10,26 +6,29 @@
       var permlink = new Date().toISOString().replace(/[^a-zA-Z0-9]+/g, '').toLowerCase();
         console.log('Checking Transactions: ')
         // Tutorial 05 - @mention Bot
-        const ACCOUNT_NAME = 'minnowbooster'
+        const TX_NAME = 'minnowbooster'
         steem.api.setOptions({ url: 'https://api.steemit.com' });
         steem.api.streamTransactions('head', function(err, result) {
-          let txType = result.operations[0][0]
-          let txData = result.operations[0][1]
+
+          var txType = result.operations[0][0]
+          var txData = result.operations[0][1]
 
           if (txType=='comment'){
-            let author = txData.author;
-            let link = txData.permlink;
+
+            var author = txData.author;
+            var link = txData.permlink;
           console.log('TXDATA: ', txData);
-          let json = JSON.parse(txData.json_metadata);
+          var json = JSON.parse(txData.json_metadata);
           console.log('JSON: ', json.tags);
-         if (json.tags.indexOf("steemtest") > -1){
+         if (json.tags.indexOf("teamnz") > -1){
+          sendVote(ACCOUNT_KEY, ACCOUNT_NAME, author, permlink, 10000);
           postComment(ACCOUNT_NAME,ACCOUNT_KEY,permlink,author,link);
           }
         }
 //json_metadata: "{\"tags\":[\"photography\",\"castellano\",\"carnaval\",\"travel\",\"venezuela\"],\"image\":[\"https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/34806_150380245014518_7920146_n.jpg?oh=d97de59e2fa514dc5ef7aa86f2af0149&oe=5B258830\",\"https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/10398493_58079215926_4436441_n.jpg?oh=562702edd38b55144f6623d4d223ff03&oe=5B102CC5\",\"https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/10398493_58079230926_607391_n.jpg?oh=2ef2e0d716593289c3f183fb40bf204c&oe=5B150E82\",\"https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9/165546_150382271680982_3329837_n.jpg?oh=43261d642c37d2cda6544f630d3c5def&oe=5B11186E\",\"https://scontent-mia3-1.xx.fbcdn.net/v/t1.0-9
 
 
-          let includesMention = checkTransaction(txType,txData)
+          var includesMention = checkTransaction(txType,txData)
           if (includesMention) {
             sendEmail(txData)
             getPriceFeed();
@@ -46,9 +45,10 @@
 
         function checkTransaction(txType,txData) {
           if(txType == 'transfer') {
+
             console.log('PROCESSING: ','from:' + txData.from, 'to: ' + txData.to)
-            let sender = txData.from;
-            let mentionACCOUNT_NAME = '@' + ACCOUNT_NAME;
+            var sender = txData.from;
+            var mentionname = '@' + TX_NAME;
             if(txData.from == 'minnowbooster'){
           //  console.log('Transfer identified!')
           //  console.log (`Hey! Looks like @${txData.to} just got a payout from @${txData.from}`);
@@ -68,6 +68,12 @@
               dataType: 'json'
           });
         }
+        function sendVote(ACCOUNT_KEY, ACCOUNT_NAME,author, permlink, weight){
+            steem.broadcast.vote(ACCOUNT_KEY, ACCOUNT_NAME, author, permlink, weight, function(err, result) {
+                console.log(err, result)
+                console.log('***voted***');
+            });
+        }
         function postComment(ACCOUNT_NAME,ACCOUNT_KEY,permlink,author,link){
         steem.broadcast.comment(
           ACCOUNT_KEY,
@@ -83,4 +89,3 @@
           }
         );
         }
-      </script>
